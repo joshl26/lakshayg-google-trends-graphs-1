e65b9e80699ad8a47d603d44a4f1fbca1cf935c7
joshl26 <joshlehman.dev@gmail.com>
2022-12-16 23:55:04 -0500

    commit

--- apibackend/server.js
+++ apibackend/server.js
@@ -1,112 +1,133 @@
-const googleTrends = require('google-trends-api');
-var express = require('express')
-var cors = require('cors')
-var app = express()
-app.use(cors())
+const googleTrends = require("google-trends-api");
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.json({
-        'Ping': 'Pong'
-    })
-})
+  res.json({
+    Ping: "Pong",
+  });
+});
 
 app.get("/:keyword/:keyword2", async (req, res) => {
+  try {
+    var result = [];
+    var result2 = [];
+    var result3 = [];
+    googleTrends
+      .interestOverTime({
+        keyword: req.params.keyword,
+      })
+      .then(function (results) {
+        // console.log((JSON.parse(results).default.timelineData[0]));
 
-    try {
+        JSON.parse(results).default.timelineData.map((data, i) => {
+          result.push({
+            date: data.formattedTime,
+            value: data.value[0],
+          });
+        });
+      })
+      .then(function () {
+        googleTrends
+          .interestOverTime({
+            keyword: req.params.keyword2,
+          })
+          .then(function (results) {
+            // console.log((JSON.parse(results).default.timelineData[0]));
 
-        //console.log('reached')
-        var result = [];
-        var result2 = [];
-        var result3 = []
-        googleTrends.interestOverTime({
-                keyword: req.params.keyword
-            })
-            .then(function(results) {
+            JSON.parse(results).default.timelineData.map((data, i) => {
+              result2.push({
+                date: data.formattedTime,
+                value: data.value[0],
+              });
+            });
+          })
+          .then(function () {
+            googleTrends
+              .interestOverTime({
+                keyword: req.params.keyword + " " + req.params.keyword2,
+              })
+              .then(function (results) {
                 // console.log((JSON.parse(results).default.timelineData[0]));
 
                 JSON.parse(results).default.timelineData.map((data, i) => {
-                    result.push({
-                        'date': data.formattedTime,
-                        'value': data.value[0]
-                    })
+                  result3.push({
+                    date: data.formattedTime,
+                    value: data.value[0],
+                  });
+                });
+                var final = new Array(result.length + 1);
+                final[0] = new Array(4);
+                final[0][0] = "Years";
+                final[0][1] = req.params.keyword;
+                final[0][2] = req.params.keyword2;
+                final[0][3] = req.params.keyword + " " + req.params.keyword2;
+                for (var i = 1; i < final.length; i++) {
+                  final[i] = new Array(4);
+                  final[i][0] =
+                    result[i - 1] && result[i - 1].date
+                      ? result[i - 1].date
+                      : "";
+                  final[i][1] = result[i - 1] && result[i - 1].value;
+                  final[i][2] =
+                    result2 && result2.length && result2[i - 1].value
+                      ? result2[i - 1].value
+                      : 0;
+                  final[i][3] =
+                    result3 && result3.length && result3[i - 1].value
+                      ? result3[i - 1].value
+                      : 0;
+                }
+                res.json(final);
+              });
+          });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/:country", async (req, res) => {
+  try {
+    var result2 = [];
 
-                })
+    await googleTrends
+      .relatedTopics({ keyword: "/m/018jqx", geo: "CA" })
+      .then(function (results) {
+        var spec = JSON.parse(results).default.rankedList[0];
 
-            }).then(function() {
-                googleTrends.interestOverTime({
-                        keyword: req.params.keyword2
-                    })
-                    .then(function(results) {
-                        // console.log((JSON.parse(results).default.timelineData[0]));
+        for (var i = 0; i < 5; i++) {
+          result2.push(spec.rankedKeyword[i].topic.title);
+        }
+        // console.log(results);
 
-                        JSON.parse(results).default.timelineData.map((data, i) => {
-                            result2.push({
-                                'date': data.formattedTime,
-                                'value': data.value[0]
-                            })
+        res.json(results);
+      });
+  } catch (err) {
+    console.log(err);
+  }
 
-                        })
+  // try  {
 
-                    }).then(function() {
-                        googleTrends.interestOverTime({
-                                keyword: req.params.keyword + " " + req.params.keyword2
-                            })
-                            .then(function(results) {
-                                // console.log((JSON.parse(results).default.timelineData[0]));
+  // var result = []
 
-                                JSON.parse(results).default.timelineData.map((data, i) => {
-                                    result3.push({
-                                        'date': data.formattedTime,
-                                        'value': data.value[0]
-                                    })
+  //         await googleTrends.dailyTrends({
+  //             geo: req.params.country
+  //         }).then(function(results) {
+  //             console.log(JSON.parse(results))
+  //             var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
+  //             for (var i = 0; i < arr.length; i++) {
+  //                 result.push(arr[i].title.query)
+  //             }
+  //             res.json(result)
+  //         })
+  //         //res.json({'trend1':trend1,'trend2':trend2})
+  //     } catch (err) {
+  //         console.log(err)
+  //     }
+});
 
-                                })
-                                var final = new Array(result.length + 1);
-                                final[0] = new Array(4);
-                                final[0][0] = "Years";
-                                final[0][1] = req.params.keyword;
-                                final[0][2] = req.params.keyword2
-                                final[0][3] = req.params.keyword + " " + req.params.keyword2;
-                                for (var i = 1; i < final.length; i++) {
-                                    final[i] = new Array(4);
-                                    final[i][0] = result[i - 1] && result[i - 1].date ? result[i - 1].date : "";
-                                    final[i][1] = result[i - 1] && result[i - 1].value;
-                                    final[i][2] = result2 && result2.length && result2[i - 1].value ? result2[i - 1].value : 0
-                                    final[i][3] = result3 && result3.length && result3[i - 1].value ? result3[i - 1].value : 0
-
-                                }
-                                res.json(final)
-
-
-                            })
-
-
-
-                    })
-            })
-
-    } catch (err) {
-        console.log(err)
-    }
-})
-app.get('/:country', async (req, res) => {
-    try {
-        var result = []
-        await googleTrends.dailyTrends({
-            geo: req.params.country
-        }).then(function(results) {
-            var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
-            for (var i = 0; i < arr.length; i++) {
-                result.push(arr[i].title.query)
-            }
-            res.json(result)
-        })
-        //res.json({'trend1':trend1,'trend2':trend2})
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-app.listen(process.env.PORT || '3001', function() {
-    console.log("Server started!!")
-})
+app.listen(process.env.PORT || "3001", function () {
+  console.log("Server started!!");
+});
